606de3ea9a912acce720e67a62af58b3720e054f
galk32 <49902643+galk32@users.noreply.github.com>
2019-05-27 00:10:37 +0300

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -24,6 +24,9 @@ Var_name <- feature_mean_std_index$V2
 
 X_test <- read.table("./test/X_test.txt")
 X_test <- as.tibble(X_test)
+subject_test <- read.table("./test/subject_test.txt")
+subject_test <- as.tibble(subject_test)
+subject_test %>% rename(subject=V1)->subject_test
 
 #choose mean and sd, variables
 x_test_mean_sd <- X_test[,feature_mean_std_index$V1]
@@ -36,17 +39,18 @@ names(x_test_mean_sd) <- Var_name
 y_test <- read.table("./test/y_test.txt")
 y_test <- as.tibble(y_test)
 
-#bind colume activity with the X_test_mean_sd_actv
-x_test_mean_sd_actv <- bind_cols(x_test_mean_sd, y_test)
-
-#rename variable V1 as activity 
+#bind colume activity and subject with the X_test_mean_sd_actv
+x_test_mean_sd_actv <- bind_cols(x_test_mean_sd, y_test,subject_test)
 x_test_mean_sd_actv %>% rename(activity = V1) -> x_test_mean_sd_actv
 
-x_test_mean_sd_actv <- x_test_mean_sd_actv[c(80,1:79)]
+#rearrange the dateset
+x_test_mean_sd_actv <- x_test_mean_sd_actv[c(81,80,1:79)]
 
-x_test_mean_sd_actv$activity <- as.factor(x_test_mean_sd_actv$activity)
-
+#change activity and subject class to be factor
 library(forcats)
+x_test_mean_sd_actv$activity <- as.factor(x_test_mean_sd_actv$activity)
+#x_test_mean_sd_actv$subject  <- as.factor(x_test_mean_sd_actv$subject)
+
 #rename factors
 x_test_mean_sd_actv$activity <- fct_recode(x_test_mean_sd_actv$activity,
                                            walking="1",walking_ustairs="2",
@@ -55,12 +59,17 @@ x_test_mean_sd_actv$activity <- fct_recode(x_test_mean_sd_actv$activity,
                                            laying="6")
 head(x_test_mean_sd_actv)
 tail(x_test_mean_sd_actv)
+
+
 #---------------------------------------------------------------------------------------------
 #-----------------------A)IMPROTING AND TIDYNG TRAIN DATA SET---------------------------------
 #---------------------------------------------------------------------------------------------
 
 X_train <- read.table("./train/X_train.txt")
 X_train <- as.tibble(X_train)
+subject_train <- read.table("./train/subject_train.txt")
+subject_train <- as.tibble(subject_train)
+subject_train %>% rename(subject=V1)->subject_train
 
 #choose mean and sd, variables
 x_train_mean_sd <- X_train[,feature_mean_std_index$V1]
@@ -73,17 +82,19 @@ names(x_train_mean_sd) <- Var_name
 y_train <- read.table("./train/y_train.txt")
 y_train <- as.tibble(y_train)
 
-#bind colume activity with the X_test_mean_sd_actv
-x_train_mean_sd_actv <- bind_cols(x_train_mean_sd, y_train)
-
-#rename variable V1 as activity 
+#bind colume activity and subject with the X_test_mean_sd_actv
+x_train_mean_sd_actv <- bind_cols(x_train_mean_sd, y_train,subject_train)
 x_train_mean_sd_actv %>% rename(activity = V1) -> x_train_mean_sd_actv
 
-x_train_mean_sd_actv <- x_train_mean_sd_actv[c(80,1:79)]
+#rearrange the dateset
+x_train_mean_sd_actv <- x_train_mean_sd_actv[c(81,80,1:79)]
 
-x_train_mean_sd_actv$activity <- as.factor(x_train_mean_sd_actv$activity)
 
+#change activity and subject class to be factor
 library(forcats)
+x_train_mean_sd_actv$activity <- as.factor(x_train_mean_sd_actv$activity)
+#x_train_mean_sd_actv$subject  <- as.factor(x_train_mean_sd_actv$subject)
+
 #rename factors
 x_train_mean_sd_actv$activity <- fct_recode(x_train_mean_sd_actv$activity,
                                            walking="1",walking_ustairs="2",
@@ -98,13 +109,16 @@ tail(x_train_mean_sd_actv)
 
 mergedataset <-bind_rows(x_train_mean_sd_actv,x_test_mean_sd_actv)
 
-variables_summary <- lapply(mergedataset, summary)
+#transforming subject to factor
+mergedataset$subject <- as.factor(mergedataset$subject)
+
+variables_summary <- sapply(mergedataset, summary)
 
 # creates a second, independent tidy data set with the average 
 # of each variable for each activity and each subject.
 
-mergedataset %>% group_by(activity) %>% 
-  summarize_all(mean) ->avg_activity
+mergedataset %>% group_by(subject,activity) %>% 
+  summarize_all(mean) -> avg_activity
 
 
 #average values per activity
